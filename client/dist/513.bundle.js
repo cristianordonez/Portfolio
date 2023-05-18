09d3ac15f48f0925b268b5f8898d3c905bf3c199
Cristian Ordonez <cristianordonezrd@gmail.com>
2023-05-18 17:24:54 -0400

    removed dist from version control system

--- client/dist/513.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkportfolio_custom_template=self.webpackChunkportfolio_custom_template||[]).push([[513],{8513:(e,t,r)=>{r.r(t),r.d(t,{default:()=>l});var o=r(2658),a=r(5893);function l(){var e=(new Date).getFullYear();return(0,a.jsx)("div",{className:"footer-container",children:(0,a.jsxs)(o.Z,{children:["Developed by Cristian Ordonez @ ",e]})})}}}]);
